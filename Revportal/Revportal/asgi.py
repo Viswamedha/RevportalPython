a21230ed0969242f1e3ae1f5887fe467535a43ca
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2022-10-03 18:50:33 +0100

    Redoing directory structure to maintain lowercase

--- Revportal/Revportal/asgi.py
+++ Revportal/Revportal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Revportal project.
+ASGI config for revportal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Revportal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'revportal.settings')
 
 application = get_asgi_application()
--- Revportal/Revportal/urls.py
+++ Revportal/Revportal/urls.py
@@ -1,4 +1,4 @@
-"""Revportal URL Configuration
+"""revportal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
